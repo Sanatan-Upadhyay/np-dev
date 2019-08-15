ba2e6dda7abfe47f88c461a40a91242e91ce9633
rock <rockcriz000@gmail.com>
2019-08-15 11:23:39 +0530

    removed duplicate code

---
+++ js/script.js
@@ -0,0 +1,80 @@
+
+// this file contain javascript code of questiuon and answer
+
+
+
+
+		$(document).ready(function(){
+		  
+
+		  // for question 1 start
+		  $("#a-1").click(function(){
+		    $("#t-1").toggle(300);
+		  });
+		  // for question 1 end
+
+
+		  // for question 2 start
+		  $("#a-2").click(function(){
+		    $("#t-2").toggle(300);
+		  });
+		  // for question 2 end
+
+
+		  // for question 3 start
+		  $("#a-3").click(function(){
+		    $("#t-3").toggle(300);
+		  });
+		  // for question 3 end
+
+
+		  // for question 4 start
+		  $("#a-4").click(function(){
+		    $("#t-4").toggle(300);
+		  });
+		  // for question 4 start
+
+
+		  // for question 5 start
+		  $("#a-5").click(function(){
+		    $("#t-5").toggle(300);
+		  });
+		  // for question 5 end
+
+
+		  // for question 6 start
+		  $("#a-6").click(function(){
+		    $("#t-6").toggle(300);
+		  });
+		  // for question 6 end
+
+
+		  // for question 7 start
+		  $("#a-7").click(function(){
+		    $("#t-7").toggle(300);
+		  });
+		  // for question 7 end
+
+
+		  // for question 8 start
+		  $("#a-8").click(function(){
+		    $("#t-8").toggle(300);
+		  });
+		  // for question 8 end
+
+
+		  // for question 9 start
+		  $("#a-9").click(function(){
+		    $("#t-9").toggle(300);
+		  });
+		  // for question 9 end
+
+
+		  // for question 10 start
+		  $("#a-10").click(function(){
+		    $("#t-10").toggle(300);
+		  });
+		  // for question 10 end
+
+		});
+		
